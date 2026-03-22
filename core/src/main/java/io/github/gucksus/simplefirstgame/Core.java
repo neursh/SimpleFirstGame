bf60018c754e0cdeea0cb7a9b7cb3ecbcb8f2806
Gucksus <130300776+Gucksus@users.noreply.github.com>
2026-03-22 09:38:13 +0700

    Add additional libraries.

--- core/src/main/java/io/github/gucksus/simplefirstgame/Core.java
+++ core/src/main/java/io/github/gucksus/simplefirstgame/Core.java
@@ -3,8 +3,10 @@ package io.github.gucksus.simplefirstgame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Core extends ApplicationAdapter {
