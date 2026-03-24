a1752bdae3e375374ba1856764c839be8acdcaac
Gucksus <130300776+Gucksus@users.noreply.github.com>
2026-03-24 15:12:30 +0700

    Fixed bullet hitbox size.

--- core/src/main/java/io/github/gucksus/simplefirstgame/entities/Bulletlv1.java
+++ core/src/main/java/io/github/gucksus/simplefirstgame/entities/Bulletlv1.java
@@ -10,18 +10,20 @@ public class Bulletlv1 {
     public Sprite selfSprite;
     Texture bulletlv1Texture;
     public Rectangle hitbox;
+    float bulletWidth = .5f;
+    float bulletHeight = .5f;
 
     public Bulletlv1(float iniX, float iniY) {
         bulletlv1Texture = new Texture("bullet_texture.png");
         selfSprite = new Sprite(bulletlv1Texture);
-        selfSprite.setSize(.5f, .5f);
+        selfSprite.setSize(bulletWidth, bulletHeight);
         selfSprite.setCenterX(iniX);
         selfSprite.setY(iniY);
-        hitbox = new Rectangle(iniX, iniY, .5f, .5f);
+        hitbox = new Rectangle(iniX + bulletWidth / 16 * 5, iniY, bulletWidth / 16 * 6, bulletHeight / 16 * 10);
     }
 
     public void update(float delta){
         selfSprite.translateY(delta * speed);
-        hitbox.setPosition(selfSprite.getX(), selfSprite.getY());
+        hitbox.setPosition(selfSprite.getX()  + bulletWidth / 16 * 5, selfSprite.getY());
     }
 }
