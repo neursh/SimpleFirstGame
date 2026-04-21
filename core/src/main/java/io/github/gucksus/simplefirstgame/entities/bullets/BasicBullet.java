ba01dcee0dcb929583adde4f39377169004929a5
Gucksus <130300776+Gucksus@users.noreply.github.com>
2026-04-21 09:43:11 +0700

    Update ship bullet and enemy bullet accordingly to the merge.

--- core/src/main/java/io/github/gucksus/simplefirstgame/entities/bullets/BasicBullet.java
+++ core/src/main/java/io/github/gucksus/simplefirstgame/entities/bullets/BasicBullet.java
@@ -1,17 +1,20 @@
 package io.github.gucksus.simplefirstgame.entities.bullets;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import io.github.gucksus.simplefirstgame.entities.base.Bullet;
 
 public class BasicBullet extends Bullet {
-    public BasicBullet(Texture texture ,float iniX, float iniY) {
-        super(texture, iniX, iniY, .5f, .5f);
+    public BasicBullet(Texture texture, float iniX, float iniY, float dx, float dy,
+            SpriteBatch batch) {
+        super(texture, iniX, iniY, .5f, .5f, dx, dy, batch);
         speed = 17f;
         damage = 1f;
         fireRate = .2f;
-        hitboxOffsetX = width / 16 * 5;
-        hitbox = new Rectangle(sprite.getX() + hitboxOffsetX, sprite.getY(), width / 16 * 6, height / 16 * 10);
+        rectangleHitboxOffset.x = width / 16 * 5;
+        rectangleHitbox = new Rectangle(sprite.getX() + rectangleHitboxOffset.x, sprite.getY(),
+                width / 16 * 6, height / 16 * 10);
         maxBulletOnScreen = 5;
     }
 }
--- core/src/main/java/io/github/gucksus/simplefirstgame/entities/bullets/SkullShooterBullet.java
+++ core/src/main/java/io/github/gucksus/simplefirstgame/entities/bullets/SkullShooterBullet.java
@@ -1,17 +1,19 @@
 package io.github.gucksus.simplefirstgame.entities.bullets;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Circle;
-import io.github.gucksus.simplefirstgame.entities.base.EnemyBullet;
+import io.github.gucksus.simplefirstgame.entities.base.Bullet;
 
-public class SkullShooterBullet extends EnemyBullet {
-    public SkullShooterBullet(Texture texture, float iniX, float iniY, float width, float height, float dx, float dy) {
-        super(texture, iniX, iniY, width, height, dx, dy);
+public class SkullShooterBullet extends Bullet {
+    public SkullShooterBullet(Texture texture, float iniX, float iniY, float width, float height,
+            float dx, float dy, SpriteBatch batch) {
+        super(texture, iniX, iniY, width, height, dx, dy, batch);
         movingType = MovingType.Straight;
         isCircle = true;
         circleHitbox = new Circle(sprite.getX(), sprite.getY(), .25f);
         speed = 5f;
-        circleHitboxOffsetX = sprite.getWidth() / 2;
-        circleHitboxOffsetY = sprite.getHeight() / 2;
+        circleHitboxOffset.x = sprite.getWidth() / 2;
+        circleHitboxOffset.y = sprite.getHeight() / 2;
     }
 }
