b9b9fcbc88317b2e1e9d1f9ede60c9a8282b6bd8
neursh <31346263+neursh@users.noreply.github.com>
2026-04-29 09:57:11 +0700

    Add scheduler to constants

--- core/src/main/java/io/github/gucksus/simplefirstgame/Constants.java
+++ core/src/main/java/io/github/gucksus/simplefirstgame/Constants.java
@@ -1,14 +1,22 @@
 package io.github.gucksus.simplefirstgame;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import io.github.gucksus.simplefirstgame.animation.AnimScheduler;
+import io.github.gucksus.simplefirstgame.maths.CubicBezier;
 import io.github.gucksus.simplefirstgame.tools.DebugRenderer;
 
 public class Constants {
     public static float worldWidth;
     public static float worldHeight;
+
     public static SpriteBatch batch;
+
     public static DebugRenderer debugRenderer;
     public static boolean debugMode;
+
+    public static AnimScheduler<CubicBezier, Vector2> cubicAnimScheduler =
+            new AnimScheduler<>(true);
 
     public static void update(float worldWidth, float worldHeight, SpriteBatch batch,
             DebugRenderer debugRenderer, boolean debugMode) {
